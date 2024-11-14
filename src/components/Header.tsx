4c119fdf66a8916344f4997d5a272c65c67c91b4
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-14 19:59:36 +0545

    added top section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,22 +5,25 @@ const Header: React.FC = () => {
   return (
     <div>
       <section className="headerSection">
-        <div className="basicInformation">
-          <div className="singleUserNavigation">
-            <span className="text-font">About Us</span>
-            <span className="text-font">My account</span>
-            <span className="text-font">Wishlist</span>
-            <span className="text-font">Order Tracking</span>
-          </div>
-          <div className="securityInfo">
-            <p className="text-font">
-              <i className="fa-solid fa-shield-halved"></i> 100% Secure delivery
-            </p>
-          </div>
-          <div className="contactInfo">
-            <p className="text-font">
-              Need help? Contact us: <span>98065542</span>
-            </p>
+        <div className="basicInformationSection">
+          <div className="basicInformation">
+            <div className="singleUserNavigation">
+              <span className="text-font">About Us</span>
+              <span className="text-font">My account</span>
+              <span className="text-font">Wishlist</span>
+              <span className="text-font">Order Tracking</span>
+            </div>
+            <div className="securityInfo">
+              <p className="text-font">
+                <i className="fa-solid fa-shield-halved"></i> 100% Secure
+                delivery
+              </p>
+            </div>
+            <div className="contactInfo">
+              <p className="text-font">
+                Need help? Contact us: <span>98065542</span>
+              </p>
+            </div>
           </div>
         </div>
         <Navbar />
