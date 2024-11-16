47371ecaabd781b3a7ab0b2f41b2480f7285d7b6
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-16 09:05:19 +0545

    added active link toggler

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,53 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import "../CSS/navbar.css";
+import {useState, useEffect } from "react";
+import Notfound from "./Notfound";
 
 const Navbar: React.FC = () => {
+
+    const [activeLink, setActiveLink] = useState<string>("home");
+    const location = useLocation();
+
+    useEffect(() => {
+        const pathname = location.pathname;
+
+        if(pathname === "/"){
+            setActiveLink("home");
+        }
+        else if(pathname === "/meat"){
+            setActiveLink("meat");
+        }
+        else if(pathname === "/bakery"){
+            setActiveLink("bakery");
+        }
+        else if(pathname === "/beverages"){
+            setActiveLink("beverages");
+        }
+        else if(pathname === "/contact"){
+            setActiveLink("contact");
+        }
+        else{
+            <Notfound/>
+        }
+    }, [location.pathname])
     return (
         <div>
             <nav className="navbar">
                 <div className="navbarContainer">
                     <ul className="navItems text-font">
-                        <li className="navLink">
+                        <li className={`navLink ${activeLink === "home" ? "activeLink" : ""}`}>
                             <NavLink to = "/" className="navLinkItem"><i className="fas fa-home"></i> Home <i className="fa-solid fa-angle-down"></i></NavLink>
                         </li>
-                        <li className="navLink">
+                        <li className={`navLink ${activeLink === "meat" ? "activeLink" : ""}`}>
                             <NavLink to = "/meat" className="navLinkItem"><i className="fas fa-drumstick-bite"></i> Meats & Seafood</NavLink>
                         </li>
-                        <li className="navLink">
+                        <li className={`navLink ${activeLink === "bakery" ? "activeLink" : ""}`}>
                             <NavLink to = "/bakery" className="navLinkItem"><i className="fas fa-cookie-bite"></i> Bakery</NavLink>
                         </li>
-                        <li className="navLink">
+                        <li className={`navLink ${activeLink === "beverages" ? "activeLink" : ""}`}>
                             <NavLink to = "/beverages" className="navLinkItem"><i className="fas fa-coffee"></i> Beverages</NavLink>
                         </li>
-                        <li className="navLink">
+                        <li className={`navLink ${activeLink === "contact" ? "activeLink" : ""}`}>
                             <NavLink to = "/contact" className="navLinkItem"><i className="fas fa-phone-alt"></i> Contact</NavLink>
                         </li>
                     </ul>
---
+++ src/components/Notfound.tsx
@@ -0,0 +1,9 @@
+const Notfound: React.FC = () => {
+    return (
+        <div>
+            <h2>Error: 404 page not found</h2>
+        </div>
+    )
+}
+
+export default Notfound;
